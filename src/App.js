1126a0e173e394ebefeb5931e466bb8226d1cc8a
it20118068 <89683180+it20118068@users.noreply.github.com>
2022-06-04 15:37:50 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles/custom-styles.css';
+import React from 'react';
 import HeaderComponent from './components/HeaderComponent';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import LoginComponent from './components/LoginComponent';
@@ -13,11 +14,12 @@ import HomeComponent from './components/HomeComponent';
 import RequestSupervisor from './components/RequestSupervisor';
 import UserProfileComponent from './components/UserProfileComponent';
 import AllocatePanel from './components/AllocatePanel';
+import RequestTopic from './components/RequestTopic';
 import StudentRequest from './components/StudentRequest';
 import StaffRequests from './components/StaffRequests';
 import MySubmissions from './components/MySubmissions';
 import StudentSubmissions from './components/StudentSubmissions';
-import ChatRoom from './components/ChatRoom';
+import ChatRoom from './components/ChatRoom'; 
 
 function App() {
 
@@ -36,37 +38,37 @@ function App() {
         <Router>
         
         <Routes>
-            {/* common */}
-            <Route exact path='/userProfile' element={<UserProfileComponent/>}/>
+                  {/* common */}
+                  <Route exact path='/userProfile' element={<UserProfileComponent/>}/>
+                  <Route exact path='/' element={<HomeComponent/>}/>
+      
+                  <Route exact path='/login' element={<LoginComponent/>}/>
 
-            <Route exact path='/login' element={<LoginComponent/>}/>
-            {/* <Route exact path='/chat' element={<ChatComponent/>}/> */}
-            <Route exact path='/Users' element={<UsersComponent/>}/>
-            <Route exact path='/register' element={<RegisterComponent/>}/>
-            <Route exact path='/file' element={<FileUploadComponent/>}/>
-            <Route exact path='/chat' element={<ChatRoom/>}/>
-
-            {/* Student */}
-            <Route exact path='/CreateStudentGroups' element={<CreateStudentGroupComponent/>}/>
-            <Route exact path='/' element={<HomeComponent/>}/>
-            <Route exact path='/request-supervisor' element={<RequestSupervisor/>}/>
-            <Route exact path='/request/topic' element={<RequestTopic/>}/>
-            <Route exact path='/request/my' element={<StudentRequest/>}/>
-            <Route exact path='/file/submissions/my' element={<MySubmissions/>}/>
-
-
-            {/* Supervisor */}
-            <Route exact path='/group-requests' element={<ResearchGroupRequests/>}/>
-            <Route exact path='/topic-requests' element={<ResearchTopicRequest/>}/>
-            <Route exact path='/request/staff' element={<StaffRequests/>}/>
-            <Route exact path='/file/submissions/students' element={<StudentSubmissions/>}/>
-
-            {/* Admin */}
-            <Route exact path='/allocate/panel' element={<AllocatePanel/>}/>
-
-
-
-        </Routes>
+                  
+                  <Route exact path='/register' element={<RegisterComponent/>}/>
+                  <Route exact path='/file' element={<FileUploadComponent/>}/>
+                  <Route exact path='/chat' element={<ChatRoom/>}/>
+      
+                  {/* Student */}
+                  <Route exact path='/CreateStudentGroups' element={<CreateStudentGroupComponent/>}/>
+                  
+                  <Route exact path='/request-supervisor' element={<RequestSupervisor/>}/>
+                  <Route exact path='/request/topic' element={<RequestTopic/>}/>
+                  <Route exact path='/request/my' element={<StudentRequest/>}/>
+                  <Route exact path='/file/submissions/my' element={<MySubmissions/>}/>
+      
+      
+                  {/* Supervisor */}
+                  <Route exact path='/request/staff' element={<StaffRequests/>}/>
+                  <Route exact path='/file/submissions/students' element={<StudentSubmissions/>}/>
+      
+                  {/* Admin */}
+                  <Route exact path='/allocate/panel' element={<AllocatePanel/>}/>
+                  <Route exact path='/Users' element={<UsersComponent/>}/>
+      
+      
+      
+              </Routes>
        
         </Router>
         </div>
